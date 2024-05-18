436b0b6bb30b508b647fb840f7cbeb1426cde1b9
Ben <ben@notionparallax.co.uk>
2024-05-18 07:18:23 +0000

    put in a condition

---
+++ photo_meta_day_3.py
@@ -0,0 +1,12 @@
+import datetime
+
+photo_meta = [
+    {
+        "filename": "made up because I lost my gopro.JPG",
+        "datetime": datetime.datetime(2024, 5, 17, 12, 12, 36),
+        "chain_vertex": True,
+        "marker_type": "numbered",
+        "marker_number": 2,
+        "bottom_condition": "unspecified",
+    }
+]
